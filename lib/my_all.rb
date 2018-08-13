b09ef15c3095711677673e9e6764f6f569c19f21
Brad McCollum <btmcco8@gmail.com>
2018-08-13 18:42:35 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,13 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  new_collection = []
   while i < collection.length
+      new_collection << yield(collection[i])
       i += 1
   end
- end
+  new_collection.include?(false) ? false : true
 end
+
+
+my_all?([1,2,3]) {|i| i < 2}
